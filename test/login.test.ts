6e09e84b98a2089956eadba14319195cc5a67841
dajungyang <djyang@tnetkorea.com>
2021-12-17 16:12:15 +0900

    Initial commit

---
+++ test/login.test.ts
@@ -0,0 +1,22 @@
+// import axios from "axios";
+// import AxiosMockAdapter from "axios-mock-adapter";
+import { authApi } from "@/__fake-api__/auth-api";
+
+// export const axiosInstance = axios.create();
+
+// const mock = new AxiosMockAdapter(axiosInstance, { delayResponse: 500 });
+
+const res = {
+    data: {
+        accessToken: `encodedHeader.encodedPayload.signature`
+    }
+};
+
+const email = "test@test.com";
+const password = "1234";
+
+// mock.onGet("/user", { params: { email: "", password: "" } });
+
+test("Login Test", () => {
+    expect(authApi.login({ email, password })).toBe(4);
+});
